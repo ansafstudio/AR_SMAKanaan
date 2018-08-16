1e5715996fd6b08421f1e1b45f72788be86d92aa
Muhammad Anwar Safei <muhammad.anwar.safei@gmail.com>
2018-08-16 15:04:12 +0700

    Prj

---
+++ Assets/Script/Game.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimplePopup;
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            SimplePopupManager.Instance.CreatePopup("Are you sure to quit?");
+            SimplePopupManager.Instance.AddButton("Yes", delegate { Application.Quit(); });
+            SimplePopupManager.Instance.AddButton("No", delegate { SimplePopupManager.Instance.HidePopup(); });
+            SimplePopupManager.Instance.ShowPopup();
+        }
+		
+	}
+}
